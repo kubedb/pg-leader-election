24250fea18a83cd4d94a2212d6e4da99bafdc379
Tamal Saha <tamal@appscode.com>
2020-05-22 19:54:08 -0700

    Update to Kubernetes v1.18.3
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- vendor/kmodules.xyz/client-go/tools/analytics/analytics.go
+++ vendor/kmodules.xyz/client-go/tools/analytics/analytics.go
@@ -1,7 +1,24 @@
+/*
+Copyright The Kmodules Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package analytics
 
 import (
 	"bytes"
+	"context"
 	"crypto/md5"
 	"encoding/hex"
 	"net"
@@ -120,14 +137,14 @@ func ClientID() string {
 	if err != nil {
 		return "$k8s$newforconfig"
 	}
-	nodes, err := client.CoreV1().Nodes().List(metav1.ListOptions{
+	nodes, err := client.CoreV1().Nodes().List(context.TODO(), metav1.ListOptions{
 		LabelSelector: "node-role.kubernetes.io/master",
 	})
 	if err != nil {
 		return reasonForError(err)
 	}
 	if len(nodes.Items) == 0 {
-		nodes, err = client.CoreV1().Nodes().List(metav1.ListOptions{
+		nodes, err = client.CoreV1().Nodes().List(context.TODO(), metav1.ListOptions{
 			LabelSelector: labels.SelectorFromSet(map[string]string{
 				"kubernetes.io/hostname": "minikube",
 			}).String(),
